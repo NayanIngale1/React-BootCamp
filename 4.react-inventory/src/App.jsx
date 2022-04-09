fd5e35ad2665a41144910d6a7ecad659346d9a27
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-04-09 23:21:02 +0530

    Inventory Created

---
+++ 4.react-inventory/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Inventory } from "./components/Inventory.jsx";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Inventory />
+    </div>
+  );
+}
+
+export default App;
