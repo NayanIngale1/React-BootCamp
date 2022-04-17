0fec2281e3743c06074ddf6bd1ab85e7af6a2b94
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-04-17 12:14:12 +0530

    updated

--- 4.1.Simple Todo/src/Components/TodoInput.jsx
+++ 4.1.Simple Todo/src/Components/TodoInput.jsx
@@ -6,13 +6,15 @@ function TodoInput({getTodo}) {
     return (
         <div className="todoInputContainer" >
             <input className="todoinput"type="text" placeholder="add todo..." onChange={(e)=>setText(e.target.value)} />
-            <button className = "addTodoBtn" onClick={() => {
-                getTodo(text);
+            <button className="addTodoBtn" onClick={() => {
+                text.length != 0 ?
+                    getTodo(text) : alert("Invalid Todo....");
             }}>ADD</button>
             
             
         </div>
     )
 }
+
 
 export {TodoInput}
--- 4.1.Simple Todo/src/Components/Todolist.jsx
+++ 4.1.Simple Todo/src/Components/Todolist.jsx
@@ -1,24 +1,41 @@
 function Todolist({todos,handleStatus,handleDelet}) {
     return (
-        <div >
-            
+      <div>
         {todos.map((e) => (
-            
-            <div className="todolistContainer">
-                <div style={e.status?{textDecorationLine: 'line-through',textDecorationColor: 'grey'}:{textDecorationLine: 'none'}}>{e.title}</div>
-                <div style={{width:"30%"}}>
-
-                <button className="doneBtn material-icons" onClick={() => {
-                    handleStatus(e.id)
-                }}>{ e.status == false ?"check":"clear"}</button>
-                <button className="deletBtn material-icons" onClick={() =>handleDelet(e.id)}>delete</button>
-                </div>
-                
+          <div className="todolistContainer">
+            <div
+              style={
+                e.status
+                  ? {
+                      textDecorationLine: "line-through",
+                      textDecorationColor: "grey",
+                      color: "#67eb09",
+                    }
+                  : { textDecorationLine: "none" }
+              }
+            >
+              {e.title}
             </div>
+            <div style={{ width: "30%" }}>
+              <button
+                className="doneBtn material-icons"
+                onClick={() => {
+                  handleStatus(e.id);
+                }}
+              >
+                {e.status == false ? "check" : "clear"}
+              </button>
+              <button
+                className="deletBtn material-icons"
+                onClick={() => handleDelet(e.id)}
+              >
+                delete
+              </button>
+            </div>
+          </div>
         ))}
-            
-        </div>
-    )
+      </div>
+    );
 }
 
 export default Todolist
